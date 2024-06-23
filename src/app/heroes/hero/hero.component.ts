9d3f4fe7aed3d2d41ca8adb44a3bb2534acaa607
Ponsiano De Loor <pdeloor@sudamericano.fin.ec>
2024-06-23 09:28:26 -0500

    manejo de datos de la interfaz hero

--- src/app/heroes/hero/hero.component.ts
+++ src/app/heroes/hero/hero.component.ts
@@ -7,4 +7,32 @@ import { Component } from '@angular/core';
 })
 export class HeroComponent {
 
+  hero = {
+    id: 1,
+    name: 'Windstorm',
+    power: 'Weather control',
+    alterEgo: 'Jane Doe',
+    universe: 'DC',
+    description: 'Windstorm is a fictional superhero appearing in American comic books published by DC Comics. The character was created by writer Gardner Fox and artist Dennis Neville, and first appeared in Flash Comics #1 in',
+    image: 'https://www.superherodb.com/pictures2/portraits/10/100/10060.jpg',
+    thumbnail: 'https://www.superherodb.com/pictures2/portraits/10/100/10060.jpg',
+    movies: ['Justice League', 'Wonder'],
+    comics: ['Flash Comics', 'Justice'],
+    age: 30
+
+  };
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  changeName() {
+    this.hero.name = 'Superman';
+  }
+
+  changeAge() {
+    this.hero.age = 35;
+  }
+
 }
