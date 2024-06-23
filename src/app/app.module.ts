0c396cc8d3b4b556b94958b171c2797ccf10fdfd
Ponsiano De Loor <pdeloor@sudamericano.fin.ec>
2024-06-23 15:02:09 -0500

    ng form con formulario para agregar y eliminar heroes usando interfaces

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { CounterComponent } from './apps/counter/counter.component';
 import { HeroComponent } from './heroes/hero/hero.component';
 import { ListComponent } from './heroes/list/list.component';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
@@ -16,7 +17,9 @@ import { ListComponent } from './heroes/list/list.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/heroes/list/list.component.ts
+++ src/app/heroes/list/list.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {Hero} from "../../interface/Hero";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-heroes-list',
@@ -6,5 +8,82 @@ import { Component } from '@angular/core';
   styleUrl: './list.component.css'
 })
 export class ListComponent {
+
+  title = 'List of heroes';
+
+  heroes:Hero[];
+  heroForm: FormGroup;
+  private fb: FormBuilder = new FormBuilder();
+
+  constructor() {
+    console.log('ListComponent created');
+    this.heroes = [
+      {
+        id: 1,
+        name: 'Spiderman',
+        power: 'Spider sense',
+        alterEgo: 'Peter Parker',
+        age: 18
+      },
+      {
+        id: 2,
+        name: 'Batman',
+        power: 'Rich',
+        alterEgo: 'Bruce Wayne',
+        age: 30
+      },
+      {
+        id: 3,
+        name: 'Superman',
+        power: 'Flying',
+        alterEgo: 'Clark Kent',
+        age: 25
+      },
+      {
+        id: 4,
+        name: 'Flash',
+        power: 'Super speed',
+        alterEgo: 'Barry Allen',
+        age: 22
+      },
+      {
+        id: 5,
+        name: 'Ironman',
+        power: 'Rich',
+        alterEgo: 'Tony Stark',
+        age: 35
+      },
+    ];
+
+    this.heroForm = this.fb.group({
+      name: ['', Validators.required, Validators.minLength(3)],
+      power: ['', Validators.required],
+      alterEgo: ['', Validators.required],
+      age: ['', Validators.required, Validators.min(1)]
+    });
+  }
+
+  ngOnInit() {
+    console.log('ListComponent initialized');
+  }
+
+  ngOnDestroy() {
+    console.log('ListComponent destroyed');
+  }
+
+  trackByFn(index: number, hero: Hero): number {
+    return hero.id;
+  }
+
+  addHero(): void {
+    const newHero: Hero = this.heroForm.value;
+    newHero.id = this.heroes.length + 1; // generate a new id
+    this.heroes.push(newHero);
+    this.heroForm.reset();
+  }
+
+  deleteHero(hero: Hero): void {
+    this.heroes = this.heroes.filter(h => h.id !== hero.id);
+  }
 
 }
